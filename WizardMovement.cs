f6219de71cf533b740267ca40be03a06c84e2c7f
Natalia <ncbarra@uc.cl>
2020-09-04 06:33:28 -0400

    Two players: Basic movement and animation

---
+++ WizardMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WizardMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    bool jump = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal_wizard") * runSpeed;
+        animator.SetFloat("Wizard_Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump_wizard")){
+            jump = true;
+        }
+    }
+
+    void FixedUpdate() {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
+        jump = false;
+    }
+}
