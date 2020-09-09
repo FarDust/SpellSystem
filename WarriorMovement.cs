caa37bfd07e335107a850e0578d812eab6f61aa2
Gabriel Faundez <gabriel.faundez@gmail.com>
2020-09-09 12:16:55 -0300

    merge master to enemies

---
+++ WarriorMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarriorMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    bool jump = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal_warrior") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump_warrior")){
+            jump = true;
+            animator.SetBool("isJumping", true);
+        }
+    }
+
+    public void OnLanding(){
+        animator.SetBool("isJumping", false);
+    }
+
+    void FixedUpdate() {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
+        jump = false;
+    }
+}
