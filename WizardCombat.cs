4dbe1ac53264857bbf827fca95102537f7fd39f6
Gabriel Faundez <gabriel.faundez@gmail.com>
2020-09-09 12:16:55 -0300

    merge master to enemies

---
+++ WizardCombat.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WizardCombat : MonoBehaviour
+{
+    public Animator animator;
+    // Solo ataca a los objetos que se encuentren en la capa enemyLayer
+    public LayerMask enemyLayer;
+
+    // Punto desde donde se efectuan los ataques
+    public Transform attackPoint;
+    // Radio del ataque, medido desde attackPoint
+    public float attackRange = 0.5f;
+    // Tiempo que se debe esperar entre ataques
+    public float attackCooldown = 0.5f;
+    // Tiempo a partir del cual puede ocurrir el siguiente ataque
+    float nextAttackTime = 0f;
+    
+    // Stats
+    public int attackDamage = 40;
+    public int maxHealth = 100;
+    int currentHealth;
+
+    void Start () {
+        currentHealth = maxHealth;
+    }
+
+    void Update(){
+        if (Time.time >= nextAttackTime){
+            if (Input.GetButtonDown("Attack_wizard")){
+                Attack();
+                nextAttackTime = Time.time + attackCooldown;
+            }
+        }
+        
+    }
+
+    void Attack(){
+        // Activa la animacion
+        animator.SetTrigger("Attack");
+        // Detectar enemigos en el rango de ataque y la capa elegida
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
+        // Dañar enemigos
+        foreach(Collider2D enemy in hitEnemies){
+            //enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+            Debug.Log("Enemy hit:" + enemy.name);
+        }
+    }
+    
+        void TakeHit(int damage){
+        animator.SetTrigger("Hurt");
+        currentHealth -= damage;
+
+        if (currentHealth <= 0){
+            Die();
+        }
+    }
+
+    void Die(){
+        animator.SetBool("isDead", true);
+    }
+
+    private void OnDrawGizmosSelected() {
+        if (attackPoint == null){
+            return;
+        }
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}
---
+++ WizardMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WizardMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    bool jump = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal_wizard") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump_wizard")){
+            jump = true;
+            animator.SetBool("isJumping", true);
+        }
+    }
+
+    public void OnLanding(){
+        animator.SetBool("isJumping", false);
+    }
+
+    void FixedUpdate() {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
+        jump = false;
+    }
+}
