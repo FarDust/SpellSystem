7b568dbe9ac9c2cff6e3bae44e874b9065d38269
Gabriel Faundez <gabriel.faundez@gmail.com>
2020-09-28 18:39:31 -0300

    feat: add fireball and healing aura to project

---
+++ SpellBook.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/SpellBook", order = 1)]
+public class SpellBook : ScriptableObject
+{
+    public string spellbookName;
+    public List<BaseSpell> spells;
+}
