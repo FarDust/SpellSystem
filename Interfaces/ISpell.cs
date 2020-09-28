daca0cbbcd2e2e113b7b8bccd055c7277d2a7ccc
Gabriel Faundez <gabriel.faundez@gmail.com>
2020-09-28 18:39:31 -0300

    feat: add fireball and healing aura to project

---
+++ Interfaces/ISpell.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ISpell
+{
+    void Cast(Transform parent, Vector3 position, Vector3 direction);
+}
---
+++ Spells/AuraSpell.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AuraSpell : BaseSpell
+{
+    public AudioClip onCreateSound;
+    public AudioClip onCastingSound;
+    public AudioClip onCancelSound;
+
+    public LayerMask targetLayers;
+    public float hitRadius;
+    public float castPeriod;
+    public float duration;
+    public GameObject spellPrefab;
+
+    public override void Cast(Transform parent, Vector3 position, Vector3 direction)
+    {
+        GameObject spell = Instantiate(spellPrefab, parent);
+        spell.transform.localScale = hitRadius * spell.transform.localScale;
+        base.Cast(parent, position, direction);
+    }
+
+    public virtual void Apply(Vector3 position)
+    {
+
+    }
+
+}
