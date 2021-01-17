ce4c5a6036f1c0236f200cf8cee7a15363feefef
Gabriel Faundez <gabriel.faundez@gmail.com>
2021-01-17 00:03:56 -0300

    refactor: clean item scripts

--- Items/StackableItem.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "StackableItem", menuName = "ScriptableObjects/Items/Stackable", order = 1)]
-public class StackableItem : Item
-{
-    
-}
